29d684cab295b008c6d8bb62b370ddd8b4025a15
wulv <wulv@youzan.com>
2018-05-02 09:34:05 +0800

    fix: add mobx

--- mobx-demo/src/App.js
+++ mobx-demo/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { observable, configure, action } from 'mobx';
+import DevTools from 'mobx-react-devtools'
+import { observer }  from 'mobx-react';
 
+configure({ enforceActions: true });
+
+@observer
 class App extends Component {
+  @observable counter = 0;
+
+  @action
+  onIncrement = () => {
+    this.counter++;
+  }
+
+  @action
+  onDecrement = () => {
+    this.counter--;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.counter}
+
+        <button onClick={this.onIncrement} type="button">Increment</button>
+        <button onClick={this.onDecrement} type="button">Decrement</button>
+        <DevTools />
       </div>
     );
   }
